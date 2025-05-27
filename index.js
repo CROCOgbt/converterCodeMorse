f10113b8ac1806ab2626bdd7b713965a3d088aff
CROCODEL <164364410+CROCOgbt@users.noreply.github.com>
2025-05-27 09:30:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ document.addEventListener("DOMContentLoaded", () => {
     outputDiv.textContent = userInput;
   });
 });
+
+(function () {
+  const _0xhex =
+    "696820616e6a6972206c75206e67617061696e20636f6e76657274206e696820636f64652079616b2c20676162757420626574";
+  const _0xmap = {
+    decode: function (hex) {
+      return Buffer.from(hex, "hex").toString("utf8");
+    },
+  };
+  const _0xlog = console;
+  _0xlog["log"](_0xmap.decode(_0xhex));
+})();
+
